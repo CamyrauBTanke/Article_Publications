fdcdb774746172b272620708fe7e8229049ac8dc
ℭαጠყթαӥ_ß_Ŧαዙҝҿ <gunko.vlad.21.09.2001a@gmail.com>
2022-10-28 04:12:24 +0300

    page question

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import {Header} from './components/sections/header/Header';
 import {Footer} from './components/sections/footer/Footer';
 import {User_section} from './components/sections/user-section/User-section';
 import {Private_policy_section} from './components/sections/privacy-policy-section/Privacy-policy-section';
-import {Question_section} from './components/sections/question-section/Question-section';
 import {Error_section} from './components/sections/error-section/Error-section';
 import {Codex_section} from './components/sections/codex-section/Codex-section';
 import {Page_catalog} from './components/pages/page-catalog/Page-catalog';
+import {Page_question} from './components/pages/page-question/Page-question'
 
 import './Setups.scss';
 import Records from './Setting.data.json';
@@ -22,11 +22,11 @@ export function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/user/:id" element={<User_section user_section_data = {data_json} />} />
-        <Route path="/catalog" element={<Page_catalog page_catalog_data = {data_json}/>}/>
+        <Route path="/catalog" element={<Page_catalog page_catalog_data = {data_json}/>} />
         <Route path="/private_policy" element={<Private_policy_section private_policy_section_data = {data_json}/>} />
-        <Route path="/question" element={<Question_section qestion_section_data = {data_json}/>}/>
+        <Route path="/question" element={<Page_question page_qustion_data={data_json}/>} />
         <Route path="/codex" element={<Codex_section codex_section_data = {data_json}/>} />
-        <Route path="/" element={<Question_section qestion_section_data = {data_json}/>} />
+        <Route path="/" element={<Page_question page_qustion_data={data_json}/>} />
         <Route path="*" element={<Error_section error_section_data = {data_json}/>} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/pages/page-question/Page-question.jsx
@@ -0,0 +1,22 @@
+import {Questions} from '../../general/Question/Question';
+import {Navigation_container} from '../../general/navigation-container/Navigation-container';
+
+export function Page_question(props) {
+    const {page_qustion_data} = props;
+    return (
+        <div className="page-section section page-question__section">
+            <div className="container page-question__container">
+                <h2 className="page-question__title">{page_qustion_data.page_question_title}</h2>
+                <a href="" className="button button-border">{page_qustion_data.page_question_create_question}</a>
+            </div>
+            <form className="container page-question__container page-question__container-flex">
+                <button name="page_question_sort_by_data" className="button button-fill">{page_qustion_data.page_question_sort_by_data}</button>
+                <button name="page_question_sort_by_popular" className="button button-fill">{page_qustion_data.page_question_sort_by_popular}</button>
+            </form>
+            <div className="container">
+                <Questions/>
+            </div>
+            <Navigation_container />
+        </div>
+    );
+}
